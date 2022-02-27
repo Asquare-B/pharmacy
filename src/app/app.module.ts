564f6d3c0a2ff52322df0bae189c87228fca5f99
Asquare-B <michal.homes11@gmail.com>
2022-02-27 12:38:18 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './myComponents/header/header.component';
+import { NavbarComponent } from './myComponents/navbar/navbar.component';
+import { FooterComponent } from './myComponents/footer/footer.component';
+import { HomeComponent } from './myComponents/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
